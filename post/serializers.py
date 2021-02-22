d43d1e35715393c4d548956fc48a08258d680143
HosseinGEEK <haviking76@gmail.com>
2021-02-22 17:15:03 +0330

    implemented post app serializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,1 +1,48 @@
-from rest_framework.serializers import ModelSerializer
+from post.models import *
+from user.serializers import *
+from rest_framework import serializers
+
+
+class PostSerializer(ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostFilesSerializer(ModelSerializer):
+    class Meta:
+        model = PostFile
+        exclude = (
+            'post',
+        )
+
+
+class PostTagSerializer(ModelSerializer):
+    user = UserProfileForPostSerializer(many=True)
+
+    class Meta:
+        model = Tag
+        exclude = (
+            'post',
+        )
+
+
+class PostLikeSerializer(ModelSerializer):
+    user = UserProfileForPostSerializer(many=True)
+
+    class Meta:
+        model = Like
+        exclude = (
+            'post',
+        )
+
+
+class PostCommentSerializer(ModelSerializer):
+    user = UserProfileForPostSerializer(many=True)
+
+    class Meta:
+        model = Comment
+        exclude = (
+            'post',
+        )
