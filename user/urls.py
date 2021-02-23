493ed6aba95bb3f9fb2650913e4818b33c1636c9
nozhan <nozhan@emtiyaz.app>
2021-02-23 22:01:57 +0330

    Change password view added

--- user/urls.py
+++ user/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from user.views import (
     Register,
     Login,
-    Profile
+    Profile,
+    ChangePassword
 
 )
 
 urlpatterns = [
     path('register', Register.as_view()),
     path('login', Login.as_view()),
-    path('profile', Profile.as_view())
+    path('profile', Profile.as_view()),
+    path('change-password', ChangePassword.as_view())
 ]
